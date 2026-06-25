0644bc192394ad295cb5288ed50a1db290f18c0c
Daniel Wolf <daniel@wolfimnetz.de>
2026-06-25 21:54:44 +0200

    sni routing w. external dg

---
+++ sni-waf-routing/sni-routing.tcl
@@ -0,0 +1,11 @@
+when CLIENTSSL_CLIENTHELLO priority 100 {
+  if {[SSL::extensions exists -type 0]} {
+    binary scan [SSL::extensions -type 0] @9a* SNI
+    if {[regexp {(?i)[^a-z0-9.-]} $SNI]} {
+      log local0. "CLIENTSSL_CLIENTHELLO client offered bogus SNI: $SNI"
+    } else {
+      virtual [class match -value -- [string tolower $SNI] equals /Common/dg_sni_map]
+      return
+    }
+  }
+}
